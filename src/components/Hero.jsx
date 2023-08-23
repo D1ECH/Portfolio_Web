f3dced4bbc1ecf947ef11e53b31cacdb27b7a759
D1ECH <diechgr48@gmail.com>
2023-08-23 21:59:08 +0200

    TERCER COMMIT. He continuado con el vídeo, empezando el modelo 3D del ordenador que se ve en la página, aunque la idea es cambiarlo por un portátil para darle mi toque y en un futuro poder hacerlo como el portfolio del restaurante de ramen. Me he quedado justo antes de instalar el paquete de three por terminal, aunque no sé qué funciona y qué no, no he probado nada.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import {motion} from 'framer-motion';
+
+import {styles} from '../styles';
+import {ComputersCanvas} from './canvas';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section classname="relative w-full h-screen mx-auto">
+      <div classname={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7x1 mx-auto flex flex-row items-start gap-5`}>
+        <div classname="flex flex-col justify-center mt-5">
+          <div classname="w-5 h-5 rounded-full bg-[#915eff]"/>
+          <div classname="w-1 sm:h-80 h-40 violet-gradient"/>
+        </div>
+
+        <div>
+          <h1 classname={`${styles.heroHeadText} text-white`}>Hi, I'm <span classname="text-[#915eff]">Diego</span></h1>
+          <p classname={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop 3D visuals, user <br classname="sm:block hidden"> interfaces and web applications</br>
+          </p>
+        </div>
+      </div>
+      
+      <ComputersCanvas>
+        
+      </ComputersCanvas>
+    </section>
   )
 }
 
