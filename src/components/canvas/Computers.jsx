9fddc6a78081d2bd6928c1aa6753ad198474b91a
D1ECH <diechgr48@gmail.com>
2023-08-24 17:52:45 +0200

    Modelo 3D puesto en marcha, lo siguiente es la sección ABOUT

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,36 +1,73 @@
-import {Suspense, useEffect, useState} from 'react'
+import {Suspense, useEffect, useState} from 'react';
 import {Canvas} from '@react-three/fiber';
 import {OrbitControls, Preload, useGLTF} from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
-const Computers = () => {
-  const computer = useGLTF('./desktop_pc/laptop_dell_latitude_5400/scene.gltf')
+const Computers = ({isMobile}) => {
+  const computer = useGLTF('./desktop_pc/scene.gltf')
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black"/>
-      <pointLight intensity={1}/>
-      <primitive>
+      <hemisphereLight intensity={0.15} 
+      groundColor="black"/>
+      <pointLight intensity={40}/>
+      <spotLight position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
         object={computer.scene}
-      </primitive>
+        scale={isMobile ? 0.7 : 0.75 }
+        position={isMobile ? [0,-3,-2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
     </mesh>
   )
 }
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    /*Añadimos un eventListener para controlar el modelo 3D cuando el 
+    tamaño de la pantalla cambia, como por ejemplo para un móvil*/
+    const mediaQuery = window.matchMedia('(max-width:500px)');
+    
+    /*Establecemos el valor inicial de nuestra variable encargada 
+    de indicarnos si es o no un móvil*/
+    setIsMobile(mediaQuery.matches);
+    
+    /*Se añade una función Listener que es llamada a actuar cuando 
+    hace falta cambiar el tamaño de la imagen 3D*/
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    }
+    
+    /*Se elimina la función Listener anterior cuando el componente 
+    no está montado*/
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    }
+  }, [])
+
   return (
     <Canvas
       frameloop="demand"
       shadows
-      camera={{position: [20,3,5], fov:20}}
+      camera={{position: [20,3,5], fov:25}}
       gl={{preserveDrawingBuffer: true}}
     >
-      <Suspense fallback={<CanvasLoader/>}> 
+      <Suspense fallback={<CanvasLoader />}> 
         <OrbitControls enableZoom={false} 
           maxPolarAngle = {Math.PI/2}
           minPolarAngle = {Math.PI/2}
         />
-        <Computers/>
+        <Computers isMobile={isMobile}/>
       </Suspense>
 
       <Preload all/>
@@ -39,4 +76,4 @@ const ComputersCanvas = () => {
   )
 }
 
-export default Computers
+export default ComputersCanvas
