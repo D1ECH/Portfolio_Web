b1691521116cd7e8adc591e778571217e00f5ae7
D1ECH <diechgr48@gmail.com>
2023-08-31 10:44:08 +0200

    Modificando la web --> Hero, Navbar y about.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,17 +10,18 @@ const Hero = () => {
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+          <div className='w-5 h-5 rounded-full bg-[#e91483]' />
+          <div className='w-1 sm:h-80 h-40 bg-gradient-to-b from-[#e91483]' />
         </div>
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
+            Hi, I'm <span className='text-[#e91483]'>Diego</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            I'm studying to develop <br className='sm:block hidden' />
+            web applications, 3D visuals... <br className='sm:block hidden' />
+            Any interesting challenge is an opportunity.
           </p>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#050816",
+        primary: "#01001e",
+        //Este es el color que había antes como primario --> #050816
         secondary: "#aaa6c3",
         tertiary: "#151030",
         "black-100": "#100d25",
@@ -19,7 +20,7 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('/src/assets/herobg.png')",
+        "hero-pattern": "url('/src/assets/77765.jpg')",
       },
     },
   },
